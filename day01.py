59b5c7064202f7dd285fc18a4d2f87317983a9ab
William de Witt <sasilverain@hotmail.com>
2018-12-02 22:05:10 +0200

    Updated for Day 1 Part 2

--- day01.py
+++ day01.py
@@ -1,14 +1,24 @@
 
-
-
 text_file = open("day01_input.txt", "r")
 input = [int(n) for n in text_file.read().split('\n')]
-
 text_file.close()
 
 answer = 0
+answer_list = []
+answer_list.append(0)
+repeat_found = False
+sum_found = False
 
-for _ in input:
-    answer += _
+while not sum_found or not repeat_found:
+    for i, v in enumerate(input):
+        answer += v
 
-print(answer)
+        if i == len(input) -1 and not sum_found:
+            print(answer)
+            sum_found = True
+        
+        if answer in answer_list and not repeat_found:
+            print(answer)
+            repeat_found = True
+
+        answer_list.append(answer)
